c8c73dda6939b2d00f1bf885eeb8a34aa60b9fed
Thomas Campistron <irevoire@hotmail.fr>
2020-03-20 18:24:37 +0100

    implements another optimisation to sum all the constants in multiple adds

--- examples/draw.rs
+++ examples/draw.rs
@@ -1,12 +1,13 @@
 use stackvm::{expr, Graph};
 
 fn main() {
-    let a = &expr::r#const(1);
+    let a = &expr::var("a");
     let b = &expr::r#const(1);
 
-    let expr = a + b;
+    let expr = &(a + b) + &(a + &(a + b));
 
     println!("digraph {{");
+    // expr.graph(0);
     expr.optimize().graph(0);
     println!("}}");
 }
--- src/expr/add.rs
+++ src/expr/add.rs
@@ -63,8 +63,57 @@ impl Add {
         let b = self.b.optimize();
 
         match (a, b) {
+            // simplify addition between constants
             (Expr::Const(a), Expr::Const(b)) => super::r#const(a.value + b.value),
+            // if your are adding zero to something, ignore the zero
+            (other, Expr::Const(c)) | (Expr::Const(c), other) if c.value == 0 => other,
+            // always put the constants first
+            (Expr::Add(expra), Expr::Add(exprb)) => match Self::merge(&expra, &exprb) {
+                (Some(c), a) => super::add(&c, &a),
+                (None, a) => a,
+            },
+            // always put the constants first c + (expr + expr)
+            (Expr::Const(c), Expr::Add(add)) | (Expr::Add(add), Expr::Const(c)) => {
+                match (&add.a, &add.b) {
+                    // c + (c + expr) => c' + expr
+                    (Expr::Const(a), b) | (b, Expr::Const(a)) => {
+                        super::add(&super::r#const(a.value + c.value), &b)
+                    }
+                    // there is no other constants: we do nothing
+                    (a, b) => super::add(&super::r#const(c.value), &super::add(&a, &b)),
+                }
+            }
+            // always put the constants first expr + (c + expr)
+            (expr, Expr::Add(add)) | (Expr::Add(add), expr) => {
+                match (&add.a, &add.b) {
+                    (c @ Expr::Const(_), b) | (b, c @ Expr::Const(_)) => {
+                        super::add(&c, &super::add(&expr, &b))
+                    }
+                    // there is no constants: we do nothing
+                    (a, b) => super::add(&expr, &super::add(&a, &b)),
+                }
+            }
+            // always put the constants first
+            (other, c @ Expr::Const(_)) => super::add(&c, &other),
+            // no optimisation were found
             (a, b) => super::add(&a, &b),
+        }
+    }
+
+    /// here we suppose all the constant are already on the left of each add
+    fn merge(a: &Add, b: &Add) -> (Option<Expr>, Expr) {
+        match (&a.a, &a.b, &b.a, &b.b) {
+            (Expr::Const(const_a), expr_a, Expr::Const(const_b), expr_b) => (
+                Some(super::r#const(const_a.value + const_b.value)),
+                super::add(&expr_a, &expr_b),
+            ),
+            (c @ Expr::Const(_), expr_a, expr_b, expr_c)
+            | (expr_a, expr_b, c @ Expr::Const(_), expr_c) => (
+                Some(c.clone()),
+                super::add(&expr_a, &super::add(&expr_b, &expr_c)),
+            ),
+            // here we do do nothing: merge the two parameters back in a add
+            (a, b, c, d) => (None, super::add(&super::add(&a, &b), &super::add(&c, &d))),
         }
     }
 }
